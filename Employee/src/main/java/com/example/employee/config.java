01b125932b754dd41f43d523eae0052ef98d716b
dilshan34 <dilshansachinthana34@gmail.com>
2022-11-18 13:29:00 +0530

    add web client

--- Employee/src/main/java/com/example/employee/config.java
+++ Employee/src/main/java/com/example/employee/config.java
@@ -2,10 +2,9 @@ package com.example.employee;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+
 public class config {
 
     @Bean
